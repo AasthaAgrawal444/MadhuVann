bb03b19fa99b663a9d545421a0b73ac815198cc5
AnugyaAKGEC <114511068+AnugyaAKGEC@users.noreply.github.com>
2023-12-09 03:52:47 -0800

    added nav bar back

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import Navbar from "./Components/Navbar.jsx";
 import FloraFauna from "./Components/Florafauna.jsx";
 
 function App() {
   return (
     <>
+      <Navbar />
       <FloraFauna />
     </>
   );
