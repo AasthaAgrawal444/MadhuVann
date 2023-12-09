83dcb1107361eaaf32bf443afd18b93162b4086e
AnugyaAKGEC <114511068+AnugyaAKGEC@users.noreply.github.com>
2023-12-09 03:41:49 -0800

    added pics for flora and fauna

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./Components/Navbar.jsx";
+
+import FloraFauna from "./Components/Florafauna.jsx";
+
 function App() {
   return (
     <>
-      <Navbar />
+      <FloraFauna />
     </>
   );
 }
+
 export default App;
