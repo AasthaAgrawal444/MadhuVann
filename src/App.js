9085152c549ffef28abf297345ada171ce5b9e32
Archit Shukla <asharchit03@gmail.com>
2023-12-11 21:44:32 +0530

    changes by ash

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ import Homepage from "./Components/homepage.js";
 import Donate from "./Components/donation/Donate.jsx";
 import FloraFauna from "./Components/Florafauna.jsx";
 
-
 function App() {
   return (
-   <div>
-   <Router>
-    <Routes>
-    <Route path="/" exact element={<Homepage />}></Route>
-    <Route path="/Donate" exact element={<Donate />}></Route>
-    <Route path="/FloraFauna" exact element={<FloraFauna />}</Route>
-
-    </Routes>
-   </Router>
-   </div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Homepage />} />
+          <Route path="/Donate" exact element={<Donate />} />
+          <Route path="/florafauna" exact element={<FloraFauna />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
 export default App;
-
