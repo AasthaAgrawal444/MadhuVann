e01c3e23b9035fd84eb2c97590ab3a538d2600da
prerna60 <135650516+prerna60@users.noreply.github.com>
2023-12-13 14:47:32 +0530

    vite

---
+++ prerna/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "green-primary": "#97B18E",
+      },
+    },
+  },
+  plugins: [],
+};
