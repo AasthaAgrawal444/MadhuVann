428a70ffa841d1178f04257dc891a91ae1040b9b
prerna60 <135650516+prerna60@users.noreply.github.com>
2023-12-14 22:52:52 +0530

    new fauna

--- madhuvann/src/App.js
+++ madhuvann/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Components/homepage.js";
 import Donate from "./Components/donation/Donate.jsx";
-import FloraFauna from "./Components/Florafauna.jsx";
+import Florafauna from "./Components/florafauna/Florafauna.jsx"
 import Flora from "./Components/flora/Flora.jsx";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Homepage />} />
           <Route path="/Donate" exact element={<Donate />} />
-          <Route path="/florafauna" exact element={<FloraFauna />} />
+          <Route path="/florafauna" exact element={<Florafauna />} />
           <Route path="/FloraFaunaCards" exact element={<Flora />} />
           <Route path='/flora' exact element={<Flora />} />
           
