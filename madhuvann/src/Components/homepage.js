10fa17557da2e9fdf5af6897aa3b2f558a6dc8b5
AnugyaAKGEC <114511068+AnugyaAKGEC@users.noreply.github.com>
2023-12-14 05:58:40 -0800

    added florafauna

--- madhuvann/src/Components/homepage.js
+++ madhuvann/src/Components/homepage.js
@@ -3,14 +3,14 @@ import Navbar from "../Components/homepage/Navbar";
 import LetsBegin from "./homepage/LetsBegin";
 import Introductory from "./homepage/Introductory";
 import IUCN from "./homepage/IUCN";
-
+import Florafauna from "./homepage/Florafauna";
 function Homepage() {
   return (
     <div>
       <Navbar />
       <LetsBegin />
       <Introductory />
-
+      <Florafauna />
       <IUCN />
     </div>
   );
