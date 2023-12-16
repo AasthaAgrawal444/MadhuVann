00b4bfaf4a3917c3afd2e1c26a18b7fcae605d1f
prerna60 <135650516+prerna60@users.noreply.github.com>
2023-12-16 20:07:57 +0530

    added fauna

--- madhuvann/src/Components/flora/Flora.jsx
+++ madhuvann/src/Components/flora/Flora.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import Navbar from "../homepage/Navbar";
+import React, { useEffect, useState } from "react";
 import Card from "../card/Card";
+import Navbar from "../homepage/Navbar";
+import "./flora.css";
 
 const CardList = () => {
   const [cards, setCards] = useState([]);
@@ -13,7 +13,7 @@ const CardList = () => {
         const response = await fetch("http://localhost:4000/api/v1/faunas");
         if (!response.ok) {
           throw new Error(
-            `Error fetching card information: ${response.statusText}`
+           ` Error fetching card information: ${response.statusText}`
           );
         }
         const { faunas } = await response.json(); // Destructure the 'faunas' property directly
@@ -32,17 +32,19 @@ const CardList = () => {
   return (
     <div>
       <Navbar />
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {cards.map(({ _id, name, scientificname, category, images }) => (
-          <Card
-            key={_id}
-            id={_id}
-            name={name}
-            scientificname={scientificname}
-            category={category}
-            images={images}
-          />
-        ))}
+      <div className="card_container">
+        <div className="container">
+          {cards.map(({ _id, name, scientificname, category, images }) => (
+            <Card
+              key={_id}
+              id={_id}
+              name={name}
+              scientificname={scientificname}
+              category={category}
+              images={images}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
